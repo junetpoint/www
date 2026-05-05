a62d7902e232c8631fe09b578c98798bed804fdd
Marcos Corrochano Arroyo <mcorrochanoa@outlook.com>
2026-05-05 22:08:38 +0200

    blog routing

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { glob } from 'astro/loaders'
+import { z } from 'astro/zod'
+import { defineCollection } from 'astro:content'
+
+const blog = defineCollection({
+  loader: glob({ base: './src/content/blog', pattern: '**/*.md' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+  })
+})
+
+export const collections = { blog }
